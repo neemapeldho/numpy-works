e904351cb5c236da06d00cc62dc68a5fb7ee4c44
Neema <neemapeldho24@gmail.com>
2025-10-26 20:10:08 +0530

    initial commit

---
+++ intro.py
@@ -0,0 +1,18 @@
+import numpy 
+
+item1 = [1,2,3,4]
+item2 = [5,6,7,8]
+
+array1 = numpy.array(item1)
+array2 = numpy.array(item2)
+
+print(array1)
+print(array2)
+
+print(array1 * array2)
+print(array1 / array2)
+print(array1 + array2)
+print(array1 - array2)
+
+# .ipynb integrated python notebook used to run code in parts(units)
+# .py python file
