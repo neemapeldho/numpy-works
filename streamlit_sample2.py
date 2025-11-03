f203f827283789aae43efdfed967257f87a1d819
Neema <neemapeldho24@gmail.com>
2025-11-03 22:03:34 +0530

    streamlit_sample2

--- streamlit_sample2.py
+++ streamlit_sample2.py
@@ -6,16 +6,21 @@ amount = []
 
 exp = streamlit.text_input("enter the item : ")
 
-amt = streamlit.text_input("enter the amount : ")
+amt = streamlit.number_input("enter the amount : ")
 
 if streamlit.button("Add expense"):
 
     expense.append(exp)
-    amount.append(int(amt))
+    amount.append(amt)
 
 print(sum(amount))
 
 if streamlit.button("Total expense"):
-    
-    streamlit.success(sum(amount))
 
+    result = 0 
+
+    for i in amount :
+         result += i 
+    
+         streamlit.success(sum(result))
+         exit()
