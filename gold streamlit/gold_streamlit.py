27c213733e6516f49bf20edb262cc025146282bd
Neema <neemapeldho24@gmail.com>
2025-11-04 15:00:39 +0530

    gold streamlit

---
+++ gold streamlit/gold_streamlit.py
@@ -0,0 +1,53 @@
+import streamlit
+import pandas
+import numpy
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+streamlit.title("GOLD PRICE AND ASSET COMPARISON DASHBOARD")
+
+data = pandas.read_csv("C:/Users/Neema P Eldho/OneDrive/Desktop/numpy works/gold streamlit/finalDataset_0208.csv")
+df = pandas.DataFrame(data)
+print(df)
+
+print(df.isna().sum())
+
+#=====================================
+
+df = df.drop(columns=["Dates","URL","FuturePrice","FutureNews"],axis=1)
+print(df)
+
+print(df.dtypes)
+
+#======================================
+
+le = LabelEncoder()
+
+df["News"] = le.fit_transform(df["News"])
+
+print(df)
+
+#=======================================
+
+X = df.iloc[:,:-1]
+Y = df.iloc[:,-1]
+
+x_train,x_test,y_train,y_test = train_test_split(X,Y,test_size=0.3)
+
+
+scaler = StandardScaler()
+scaler.fit_transform(x_train)
+scaler.transform(x_test)
+
+knn = KNeighborsClassifier()
+knn = knn.fit(x_train,y_train)
+
+y_pred = knn.predict(x_test)
+
+print(accuracy_score(y_pred,y_test))
+
+#========================================
+
