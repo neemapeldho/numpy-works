a7d9de2590981e93e326d22c812732c3acfc8794
Neema <neemapeldho24@gmail.com>
2025-11-07 14:09:38 +0530

    gold dataset streamlit

--- gold streamlit/gold_streamlit.py
+++ gold streamlit/gold_streamlit.py
@@ -24,12 +24,23 @@ print(df.dtypes)
 
 #======================================
 
+print(df["News"].max())
+print(df["Price or Not"].max())
+print(df["Direction Up"].max())
+print(df["Direction Constant"].max())
+print(df["Direction Down"].max())
+print(df["PastPrice"].max())
+print(df["PastNews"].max())
+
+#======================================
+
 le = LabelEncoder()
 
 df["News"] = le.fit_transform(df["News"])
 
 print(df)
 
+print(df.dtypes)
 #=======================================
 
 X = df.iloc[:,:-1]
@@ -49,18 +60,41 @@ y_pred = knn.predict(x_test)
 
 print(accuracy_score(y_pred,y_test))
 
-print(df.describe)
 
 #========================================
 
+# max,min needed to find 
 
-# def input_features():
+def input_features():
 
-#     price = streamlit.number_input("enter the price : ")
-#     direction_up = streamlit.number_input("enter the direction up : ")
-#     direction_constant = streamlit.number_input("enter the direction constant : ")
-#     direction_down = streamlit.number_input("enter the direction down : ")
-#     past_price = streamlit.number_input("enter the past_price : ")
-#     past_news = streamlit.number_input("enter the past news : ")
+    news = streamlit.text_input("enter the news :  ",)
+    price = streamlit.number_input("enter the price : ")
+    direction_up = streamlit.number_input("enter the direction up : ")
+    direction_constant = streamlit.number_input("enter the direction constant : ")
+    direction_down = streamlit.number_input("enter the direction down : ")
+    past_price = streamlit.number_input("enter the past_price : ")
+    past_news = streamlit.number_input("enter the past news : ")
 
 
+    data1 = {
+        "News " :news,
+        "Price or Not" : price,
+        "Direction Up" : direction_up,
+        "Direction Constant" :  direction_constant,
+        "Direction Down" : direction_down,
+        "PastPrice" : past_price,
+        "PastNews" : past_news,
+    }
+
+    features = pandas.DataFrame(data1,index=[0])
+    return features
+        
+input_df = input_features()
+input_scaled = scaler.transform(input_df)
+result = knn.predict(input_scaled)
+
+if streamlit.button("show result"):
+
+    final_result = "Asset Comparision is there" if result[0] == 0 else "Not Asset Comparision" # single row [0]
+
+    streamlit.success(final_result)
