6b6a694b9b29eff2fdf6bb3fbf1f48b0ec914b4b
Neema <neemapeldho24@gmail.com>
2025-11-28 15:09:01 +0530

    updated

---
+++ hyperparameter/intro.py
@@ -0,0 +1,17 @@
+"""
+hyper parameter tuning
+=====================
+ >>> the settings which are given to model before training 
+ >>> they decide how the model learns and performs
+
+
+ KNN   k = 5           k = 7 
+ random_forest   n_estimators = 100    300  1000
+
+ Trying different combinations of hyper parameters and selecting the best one
+
+
+"""
+
+
+
